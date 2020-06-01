746c1abad72554b720289421a66a15f804f730f5
Somsak Binarwaeloh <bsomsak.dev@gmail.com>
2020-06-01 15:43:25 +0700

    complete secion-2, no-17 (Model Objects Methods)

--- demo/views.py
+++ demo/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from .models import Book
+
 
 class Another(View):
+    book = Book.objects.get(id=1)
+    output = f"We have {book.title} books in DB with book ID {book.id}<br>"
+
+    # for book in books:
+    #     output += f"We have {book.title} books in DB with book ID {book.id}<br>"
 
     def get(self, request):
-        return HttpResponse('This is another function inside class')
+        return HttpResponse(self.output)
+
 
 def first(request):
     return HttpResponse("First message from views")
