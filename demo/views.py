33bbda248a5e41acd80a9eb18fcb7b3002482efb
Somsak Binarwaeloh <bsomsak.dev@gmail.com>
2020-06-01 16:10:09 +0700

    complete secion-2, no-19 (Dynamics template)

--- demo/views.py
+++ demo/views.py
@@ -6,4 +6,5 @@ from django.shortcuts import render
 
 
 def first(request):
-    return render(request, 'first_temp.html')
+    books = Book.objects.all()
+    return render(request, 'first_temp.html', {'books': books})
