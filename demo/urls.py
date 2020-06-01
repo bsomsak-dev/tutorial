531501b377a99bcad546c27acb718bffe080e628
Somsak Binarwaeloh <bsomsak.dev@gmail.com>
2020-06-01 15:56:47 +0700

    complete secion-2, no-18 (Templates)

--- demo/urls.py
+++ demo/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import Another
+
 
 urlpatterns = [
     path('first', views.first),
-    path('another', Another.as_view())
 ]
--- demo/views.py
+++ demo/views.py
@@ -2,18 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from .models import Book
-
-
-class Another(View):
-    book = Book.objects.get(id=1)
-    output = f"We have {book.title} books in DB with book ID {book.id}<br>"
-
-    # for book in books:
-    #     output += f"We have {book.title} books in DB with book ID {book.id}<br>"
-
-    def get(self, request):
-        return HttpResponse(self.output)
+from django.shortcuts import render
 
 
 def first(request):
-    return HttpResponse("First message from views")
+    return render(request, 'first_temp.html')
