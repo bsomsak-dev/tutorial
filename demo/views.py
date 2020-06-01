3a1bee85b70a1487a1f4c734bf75037bc209552c
Somsak Binarwaeloh <bsomsak.dev@gmail.com>
2020-06-01 15:10:14 +0700

    complete secion-2, no-15 (Class Views)

--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
+class Another(View):
 
-# Create your views here.
+    def get(self, request):
+        return HttpResponse('This is another function inside class')
+
 def first(request):
     return HttpResponse("First message from views")
